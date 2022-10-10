9270d72a7eac9faca0643c700e556e5bd0db71b4
zong400 <zong400@users.noreply.github.com>
2022-10-10 11:39:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,11 +11,11 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # Add alist
-git clone https://github.com/alist-org/alist.git package/alist
+#git clone https://github.com/alist-org/alist.git package/alist
