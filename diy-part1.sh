91e43bac8bf340d1f8a673d73d877c15ee362983
zong400 <zong400@users.noreply.github.com>
2022-10-11 09:41:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # Add alist
 git clone https://github.com/alist-org/alist.git package/alist
