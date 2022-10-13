c5745d6ea3e5fe1fedd48867a23072f80504ea1f
zong400 <zong400@users.noreply.github.com>
2022-10-13 13:40:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,9 +14,9 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # Add alist
-#git clone https://github.com/alist-org/alist.git package/alist
-#git clone https://github.com/sbwml/luci-app-alist package/alist
+git clone https://github.com/alist-org/alist.git package/alist
+git clone https://github.com/sbwml/luci-app-alist package/alist
